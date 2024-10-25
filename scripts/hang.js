9783fec133759908318b30777fbd633139463f7f
Maya Eid <mayabaseleid@gmail.com>
2024-10-26 02:47:08 +0300

    Add hidden class

--- scripts/hang.js
+++ scripts/hang.js
@@ -3,11 +3,14 @@ var pressedKey = "";
 document.addEventListener("keypress",(e)=>{
     pressedKey = e.key.toLowerCase();
     console.log(pressedKey);
-
+//meteor
     for(const char of randomWord){
         if(char === pressedKey){
             const letterIndex = letters.indexOf(char);
             dashes[letterIndex] = char;
+        }
+        else{
+            document.querySelector(".head").classList.remove("hidden");
         }  
     }
 
